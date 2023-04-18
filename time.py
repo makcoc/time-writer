0c49c71dfa5a77bf6f0679eaf73b49808708369d
Maksim Makarov <89390951+makcoc@users.noreply.github.com>
2023-04-18 17:27:22 +0300

    Update time.py

--- time.py
+++ time.py
@@ -1,9 +1,8 @@
 import datetime
 a = 1
 t = datetime.datetime(1,1,1,0,0,0)
-with open("output.txt", "a") as f:
-        while a < 250:
-                print(t.strftime("%M:%S"), end= ', ', file=f)
-                a +=1
-                t = t + datetime.timedelta(0,1)
+while a < 250:
+       print(t.strftime("%M:%S"), end= ', ')
+       a +=1
+       t = t + datetime.timedelta(0,1)
 
